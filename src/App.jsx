0182acafacd27df19a252be22b50ade991354932
fitrianurfa <fitrianurfatmawati701@gmail.com>
2023-07-15 18:47:43 +0700

    test-suitmedia

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ContactUs from './components/contactUs'
+import Footer from './components/Footer'
+import OurValue from './components/OurValue'
+import Navbar from './components/Navbar'
+import Slider from './components/Slider'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Slider />
+    <OurValue/>
+    <ContactUs />
+    <Footer/>
+    </>
+  )
+}
+
+export default App
